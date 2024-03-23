24fdd0c16761a5edaafb858e88ad73dd44342f9a
RICCOM <ericomun@gmail.comgit>
2024-03-23 21:58:35 +0300

    add intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+//  create cats array
+let cats = ["Milo", "Otis", "Garfield"];
+//add a cat at the end of the array
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  //add a cat at the start of the cats array
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  //remove the last cat from the array
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  //remove the first cat from the array
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  //create anew array with the cats array attached at the end
+  function appendCat(name) {
+    return [...cats, name];
+  }
+  //create a new array with the cats array starting
+  function prependCat(name) {
+    return [name, ...cats];
+  }
+  //remove the last cat  from the new array
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  //remove the first cat from the new array
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
